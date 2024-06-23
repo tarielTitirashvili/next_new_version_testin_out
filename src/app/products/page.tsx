8281d1a6f65bbbaa1ecd40c5d99c820707f9ad20
tariel <tarieltitirashvili@gmail.com>
2024-06-23 04:22:06 -0700

    add products page with blog page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+type Props = {}
+
+const Products = (props: Props) => {
+  return (
+    <div>
+      <h1>Products list</h1>
+      <h1>Product 1</h1>
+      <h1>Product 2</h1>
+      <h1>Product 3</h1>
+      <h1>Product 4</h1>
+    </div>
+  )
+}
+
+export default Products
