3de4a1a59c68678a240903945daa6fee8248e6b0
tariel <tarieltitirashvili@gmail.com>
2024-07-09 06:37:46 -0700

    nav buttons for reviews

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import BackButton from "../../../components/buttons/backButton"
+import Link from "next/link"
 
 type Props = {
   params: {
@@ -12,6 +13,20 @@ const ProductId = (props: Props) => {
     <div>
       <h1>product details page {props.params.productId}</h1>
       <BackButton />
+      <div>
+        <h1>
+          <Link href={`${props.params.productId}/reviews/2`}>review 1</Link>
+        </h1>
+        <h1>
+          <Link href={`${props.params.productId}/reviews/3`}>review 2</Link>
+        </h1>
+        <h1>
+          <Link href={`${props.params.productId}/reviews/4`}>review 3</Link>
+        </h1>
+        <h1>
+          <Link href={`${props.params.productId}/reviews/5`}>review 4</Link>
+        </h1>
+      </div>
     </div>
   )
 }
