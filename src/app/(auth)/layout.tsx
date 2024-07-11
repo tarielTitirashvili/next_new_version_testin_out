822f535a0d38eddea26eaf2d27682ea1647ba071
tariel <tarieltitirashvili@gmail.com>
2024-07-11 07:26:27 -0700

    navigation with active route highlighting

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,20 +1,48 @@
+"use client"
 import { Metadata } from "next"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 import React from "react"
+import css from './styles.module.css'
 
-export const metadata: Metadata = {
-  title: {
-    default: "Auth",
-    template: "%s | Auth",
-  },
+// export const metadata: Metadata = {
+//   title: {
+//     default: "Auth",
+//     template: "%s | Auth",
+//   },
+// }
+
+type TRoutes = {
+  title: string
+  navLink: string
 }
+
+const navLinks: TRoutes[] = [
+  { title: "Login", navLink: "/login" },
+  { title: "Register", navLink: "/register" },
+  { title: "Forget Password", navLink: "/forget-password" },
+]
 
 type Props = {
   children: React.ReactNode
 }
 
 function AuthLayout({ children }: Props) {
+  const pathName = usePathname()
+  console.log(pathName)
+
   return (
     <div>
+      <div className={css.navLinksContainer}>
+        {navLinks.map(({ title, navLink }) => {
+          const isActive = pathName.startsWith(navLink)
+          return (
+            <Link href={navLink} key={navLink} className={`${isActive ? css.active : ''}`}>
+              {title}
+            </Link>
+          )
+        })}
+      </div>
       <h1> tariel </h1>
       {children}
     </div>
